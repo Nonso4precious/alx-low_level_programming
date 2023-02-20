959f7d68731cf1c167ca0b327ceaca43379316e4
Nonso4precious <nonso4precious@gmail.com>
2023-02-19 18:55:03 -0800

    My corrected last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A c program that prints a line in standard error
- * Return 1 (Success)
+ *main - A c program that prints a line in standard error
+ *Return: Returns 1
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n", stdout);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n", 59);
 	return (1);
 }
