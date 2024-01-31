b7ec23ac3f13fd9f281ac092da738666f80bf139
Nonso4precious <nonso4precious@gmail.com>
2024-01-31 20:18:00 +0100

    My task 2 on 0x18 dynamic lib

---
+++ 0x18-dynamic_libraries/operator.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*
+ * add function
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/*
+ * division function
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/*
+ * multiplication function
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/*
+ * mod function
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+/*
+ * subtract
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
